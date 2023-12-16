ab69d7a7476c30431e12d7b04ffe343de8d28d91
Klexup <jameshensley37@gmail.com>
2023-12-15 18:10:20 -0700

    moved mainpage logic into todoContext

--- src/contexts/todoContext.jsx
+++ src/contexts/todoContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
 export const TodoContext = createContext();
@@ -13,6 +13,97 @@ export default function TodoProvider({ children }) {
     handleDeleteTask,
   ] = useLocalStorage("todos");
 
+  const [searchInput, setSearchInput] = useState("");
+  const [tagFilter, setTagFilter] = useState(null);
+  const [filter, setFilter] = useState("default");
+
+  let allCurrentTags = [];
+  todos.forEach((element) => {
+    if (element.tags.length > 0) {
+      const tagArray = element.tags;
+      for (let value of tagArray) {
+        if (!allCurrentTags.includes(value)) {
+          allCurrentTags.push(value);
+        }
+      }
+    }
+  });
+
+  let todosCopy = [...todos];
+  if (searchInput !== "") {
+    todosCopy = todosCopy.filter((value) =>
+      value.taskName.includes(searchInput),
+    );
+  }
+  if (tagFilter !== null) {
+    todosCopy = todosCopy.filter((value) => value.tags.includes(tagFilter));
+  }
+  function filterTodos() {
+    switch (filter) {
+      case "default":
+        return todosCopy;
+      case "ascending-priority":
+        return todosCopy.sort((a, b) => {
+          const valueA = a.priorityLevel;
+          const valueB = b.priorityLevel;
+          if (valueA < valueB) {
+            return -1;
+          } else if (valueA > valueB) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      case "descending-priority":
+        return todosCopy.sort((a, b) => {
+          const valueA = a.priorityLevel;
+          const valueB = b.priorityLevel;
+          if (valueA < valueB) {
+            return 1;
+          } else if (valueA > valueB) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      case "ascending-complexity":
+        return todosCopy.sort((a, b) => {
+          const valueA = a.complexityLevel;
+          const valueB = b.complexityLevel;
+          if (valueA < valueB) {
+            return -1;
+          } else if (valueA > valueB) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      case "descending-complexity":
+        return todosCopy.sort((a, b) => {
+          const valueA = a.complexityLevel;
+          const valueB = b.complexityLevel;
+          if (valueA < valueB) {
+            return 1;
+          } else if (valueA > valueB) {
+            return -1;
+          } else {
+            return 0;
+          }
+        });
+      case "ascending-date":
+        return todosCopy.sort(
+          (a, b) => new Date(a.dueDate) - new Date(b.dueDate),
+        );
+      case "descending-date":
+        return todosCopy.sort((a, b) => {
+          return new Date(b.dueDate) - new Date(a.dueDate);
+        });
+
+      default:
+        return todos;
+    }
+  }
+
   return (
     <>
       <TodoContext.Provider
@@ -23,6 +114,14 @@ export default function TodoProvider({ children }) {
           handleUpdateTask,
           toggleCompleted,
           handleDeleteTask,
+          filter,
+          setFilter,
+          tagFilter,
+          setTagFilter,
+          searchInput,
+          setSearchInput,
+          allCurrentTags,
+          filterTodos,
         }}
       >
         {children}
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,4 +1,4 @@
-import { useContext, useRef, useState, Fragment } from "react";
+import { useContext, useRef, useState } from "react";
 import SortDropdown from "../components/SortDropdown";
 import CategoryDropdown from "../components/CategoryDropdown";
 import { Link } from "react-router-dom";
@@ -6,99 +6,25 @@ import { TodoContext } from "../contexts/todoContext";
 import Todo from "../components/Todo";
 
 export default function MainPage() {
-  const { todos, setTodos, toggleCompleted } = useContext(TodoContext);
+  const {
+    todos,
+    setTodos,
+    handleSubmitNewTask,
+    handleUpdateTask,
+    toggleCompleted,
+    handleDeleteTask,
+    filter,
+    setFilter,
+    tagFilter,
+    setTagFilter,
+    searchInput,
+    setSearchInput,
+    allCurrentTags,
+    filterTodos,
+  } = useContext(TodoContext);
+
   const [searchFocused, setSearchFocused] = useState(false);
   const searchRef = useRef();
-  const [tagFilter, setTagFilter] = useState(null);
-  const [filter, setFilter] = useState("default");
-  const [searchInput, setSearchInput] = useState("");
-
-  let allCurrentTags = [];
-  todos.forEach((element) => {
-    if (element.tags.length > 0) {
-      const tagArray = element.tags;
-      for (let value of tagArray) {
-        if (!allCurrentTags.includes(value)) {
-          allCurrentTags.push(value);
-        }
-      }
-    }
-  });
-
-  let todosCopy = [...todos];
-  if (searchInput !== "") {
-    todosCopy = todosCopy.filter((value) =>
-      value.taskName.includes(searchInput),
-    );
-  }
-  if (tagFilter !== null) {
-    todosCopy = todosCopy.filter((value) => value.tags.includes(tagFilter));
-  }
-  function filterTodos() {
-    switch (filter) {
-      case "default":
-        return todosCopy;
-      case "ascending-priority":
-        return todosCopy.sort((a, b) => {
-          const valueA = a.priorityLevel;
-          const valueB = b.priorityLevel;
-          if (valueA < valueB) {
-            return -1;
-          } else if (valueA > valueB) {
-            return 1;
-          } else {
-            return 0;
-          }
-        });
-      case "descending-priority":
-        return todosCopy.sort((a, b) => {
-          const valueA = a.priorityLevel;
-          const valueB = b.priorityLevel;
-          if (valueA < valueB) {
-            return 1;
-          } else if (valueA > valueB) {
-            return -1;
-          } else {
-            return 0;
-          }
-        });
-      case "ascending-complexity":
-        return todosCopy.sort((a, b) => {
-          const valueA = a.complexityLevel;
-          const valueB = b.complexityLevel;
-          if (valueA < valueB) {
-            return -1;
-          } else if (valueA > valueB) {
-            return 1;
-          } else {
-            return 0;
-          }
-        });
-      case "descending-complexity":
-        return todosCopy.sort((a, b) => {
-          const valueA = a.complexityLevel;
-          const valueB = b.complexityLevel;
-          if (valueA < valueB) {
-            return 1;
-          } else if (valueA > valueB) {
-            return -1;
-          } else {
-            return 0;
-          }
-        });
-      case "ascending-date":
-        return todosCopy.sort(
-          (a, b) => new Date(a.dueDate) - new Date(b.dueDate),
-        );
-      case "descending-date":
-        return todosCopy.sort((a, b) => {
-          return new Date(b.dueDate) - new Date(a.dueDate);
-        });
-
-      default:
-        return todos;
-    }
-  }
 
   return (
     <>
